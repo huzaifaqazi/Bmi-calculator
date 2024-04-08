deb1564d1c0c856a3461a71812dba4d809e7bc51
Huzaifa Qazi <160002762+huzaifaqazi@users.noreply.github.com>
2024-04-08 23:41:45 +0500

    create a bmi calculator

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// bmi calculator
+let weigth = 58;
+let heigth = 2.5;
+let bmi = weigth / (heigth * heigth);
+console.log(`your weigth is ${bmi}`);
---
+++ main.ts
@@ -0,0 +1,5 @@
+// bmi calculator
+let weigth=58;
+let heigth=2.5;
+let bmi= weigth/ (heigth*heigth);
+console.log(`your weigth is ${bmi}`);
